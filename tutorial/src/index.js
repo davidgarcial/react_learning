b8ae6d0922130384a2745182e976e6e46b5c64c2
davidgarcial <d.alex10092@gmail.com>
2021-05-22 12:39:02 -0500

    Add content

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+/*
 function Greeting() {
-  return <h4>This is David an this is my first component</h4>
+ return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  )
+}
+*/
+
+const Greeting = () => {
+  return React.createElement('h1',{},'hello world');
 }
 
 ReactDom.render(<Greeting/>, document.getElementById('root'));
